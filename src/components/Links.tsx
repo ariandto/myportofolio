16c9fabec37427efb66b6a9d2ce7a3edcfe356e9
ariandto <92358210+ariandto@users.noreply.github.com>
2025-06-04 01:46:04 +0700

    versi 2

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -7,18 +7,18 @@ const Links: React.FC = () => {
       data-aos="fade-left"
       className="absolute right-[0%] top-0 mt-3 flex w-full items-center gap-5 selection:bg-AlmostWhite selection:text-AlmostBlack lg:-right-28 lg:bottom-[100px] lg:top-auto lg:mt-0 lg:h-full lg:w-fit lg:translate-x-0 lg:flex-col lg:justify-end lg:gap-14"
     >
-      <li className="h-fit w-fit text-center text-xs hover:underline sm:text-xs md:text-base lg:rotate-90">
-        <Link to={"https://medium.com/@ridloghfry"} target="_blank">
+      {/* <li className="h-fit w-fit text-center text-xs hover:underline sm:text-xs md:text-base lg:rotate-90">
+        <Link to={""} target="_blank">
           Medium
         </Link>
-      </li>
+      </li> */}
       <li className="h-fit w-fit text-center text-xs hover:underline sm:text-xs md:text-base lg:rotate-90">
-        <Link to={"https://github.com/RidloGhifary"} target="_blank">
+        <Link to={"https://github.com/ariandto/"} target="_blank">
           Github
         </Link>
       </li>
       <li className="h-fit w-fit text-center text-xs hover:underline sm:text-xs md:text-base lg:rotate-90">
-        <Link to={"https://www.linkedin.com/in/ridlo-ghifary/"} target="_blank">
+        <Link to={"https://www.linkedin.com/in/budi-ariyanto-7a4564199/"} target="_blank">
           LinkedIn
         </Link>
       </li>
--- src/components/SkillList.tsx
+++ src/components/SkillList.tsx
@@ -14,20 +14,13 @@ const SkillList: React.FC = () => {
     { src: "https://skillicons.dev/icons?i=nextjs", alt: "nextjs logo" },
     { src: "https://skillicons.dev/icons?i=react", alt: "react logo" },
     { src: "https://skillicons.dev/icons?i=vite", alt: "vite logo" },
-    { src: "https://skillicons.dev/icons?i=redux", alt: "redux logo" },
     { src: "https://skillicons.dev/icons?i=tailwind", alt: "tailwindcss logo" },
     { src: "https://skillicons.dev/icons?i=bootstrap", alt: "bootstrap logo" },
     { src: "https://skillicons.dev/icons?i=git", alt: "git logo" },
-    { src: "https://skillicons.dev/icons?i=mongodb", alt: "mongodb logo" },
     { src: "https://skillicons.dev/icons?i=firebase", alt: "firebase logo" },
     { src: "https://skillicons.dev/icons?i=mysql", alt: "mysql logo" },
-    { src: "https://skillicons.dev/icons?i=postgres", alt: "postgresql logo" },
-    { src: "https://skillicons.dev/icons?i=supabase", alt: "supabase logo" },
     { src: "https://skillicons.dev/icons?i=express", alt: "express logo" },
     { src: "https://skillicons.dev/icons?i=nodejs", alt: "nodejs logo" },
-    { src: "https://skillicons.dev/icons?i=prisma", alt: "prisma logo" },
-    { src: "https://skillicons.dev/icons?i=docker", alt: "docker logo" },
-    { src: "https://skillicons.dev/icons?i=redis", alt: "redis logo" },
   ];
 
   return (
