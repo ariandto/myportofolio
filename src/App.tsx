2c5ab694e217f670225078082cac09f5ac0083de
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-16 03:20:05 +0700

    delete axios not declare

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import axios from "axios";
 
 // Components
 import BackgroundPattern from "./components/BackgroundPattern";
