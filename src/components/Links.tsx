9d28a6c4f80959f2a11f4f813856878567e0b2fb
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-19 00:23:26 +0700

    asd

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Links: React.FC = () => {
   return (
-    <ul className="flex gap-4 items-center text-white">
+    <ul className="flex items-center gap-4 text-white">
       {/* GitHub */}
       <li>
         <a
           href="https://github.com/ariandto"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-gray-300 transition"
+          className="transition hover:text-gray-300"
         >
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+          <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
             <path d="M12 .3a12 12 0 00-3.8 23.4c..." />
           </svg>
         </a>
@@ -23,9 +23,9 @@ const Links: React.FC = () => {
           href="https://www.linkedin.com/in/budi-ariyanto-7a4564199/"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-blue-400 transition"
+          className="transition hover:text-blue-400"
         >
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+          <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
             <path d="M20.45 20.45h-3.6v-5.6..." />
           </svg>
         </a>
