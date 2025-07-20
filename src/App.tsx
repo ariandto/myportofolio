9cdbac4fd7c698f5cc4658b231d45e3a061d5604
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-20 22:22:40 +0700

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ThemeSwitcher from "./components/ThemeSwitcher";
 
 // Components
 import BackgroundPattern from "./components/BackgroundPattern";
